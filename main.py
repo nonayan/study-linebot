8e804af35edc455e5c3c7401e08b03b627e74151
nonayan <kousuke20001219@icloud.com>
2022-07-31 15:22:11 +0900

    random完成

--- main.py
+++ main.py
@@ -64,12 +64,10 @@ def handle_message(event):
     num = count -1
 
     rand = random.uniform(0, 3)
+    
+    String = [["あ", "い", "う", "え"], ["それ", "れん", "もり", "わだ"], ["しばた", "ノナカ", "ほかぜ", "たけし"], ["すずむし", "れんこん", "たいよう", "コーヒー"]]
 
-   # String = [["あ", "い", "う", "え"], ["それ", "れん", "もり", "わだ"], ["しばた", "ノナカ", "ほかぜ", "たけし"], ["すずむし", "れんこん", "たいよう", "コーヒー"]]
-
-    String_1 = ["あ", "愛", "上い", "それだよ"]
-
-    reply_message = String_1[num]
+    reply_message = String[num][rand]
 
     line_bot_api.reply_message(
         event.reply_token,
