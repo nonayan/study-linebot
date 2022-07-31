850fdb9e906d563368e7a2c4afb944e3b62ec40d
nonayan <kousuke20001219@icloud.com>
2022-07-31 14:39:17 +0900

    third commit

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=f"あなたは"event.message.text"と言いました。)) #ここでオウム返しのメッセージを返します。
+        TextSendMessage(text=f"あなたは{event.message.text}と言いました。")) #ここでオウム返しのメッセージを返します。
  
 # ポート番号の設定
 if __name__ == "__main__":
