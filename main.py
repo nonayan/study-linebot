a81ab2939fea8b4eaf8f266f1c41755e77e1e5cf
nonayan <kousuke20001219@icloud.com>
2022-08-05 21:44:49 +0900

    内容の変更

--- main.py
+++ main.py
@@ -65,10 +65,40 @@ def handle_message(event):
 
     rand = random.randint(0, 4)
     
-    String = [["ま", "!", "?", "り", "え"], ["マジ", "ガチ", "KP", "なえ", "りょ"], ["イエベ", "ブルベ", "ズッ友", "最&高", "バおわ"], ["キャパい", "アセアセ", "イケてる", "ギャルべ", "あげぽよ"], ["やりらフィ", "こみこみで", "時差グラム", "生きるww", "大丈夫そ?"], ["初心LOVE", "タンバルモリ", "ギャルピース", "片思いハート", "ちゅきちゅき"], ["わかりみが深い", "パワー!ヤー!", "ルーズソックス", "ガチおわなんよ", "ふざけんなよ!"], ["フレンチガーリー", "超ちるなラッパー", "どうかしてるぜっ", "あげていこうぜ!", "コーヒー無糖で。"], ["イーアールサンスー", "アーニャピーナッツ", "人生詰んだじゃんw", "元カレがさ~クズ!", "ウチ辛いのマヂ無理"], ["ギャルちょーかわいー", "ひよってるやついる?", "パーティーしちゃう?", "ちょーかわいーじゃん", "キュンさせてもろて。"]]
-    
-    if num <= 9:
-        reply_message = String[num][rand]
+    String_1 = ["は？", "なに？", "かまちょかよ", "なんやねん"]
+    String_2 = ["なんやねん", "ガチ", "KP", "なえ", "りょ"]
+    String_3 = ["黙れ", "ボケー", "ズッ友", "最&高", "バおわ"]
+    String_4 = ["キャパい", "アセアセ", "イケてる", "ギャルべ", "あげぽよ"]
+    String_5 = ["やりらフィ", "こみこみで", "時差グラム", "生きるww", "大丈夫そ?"]
+    String_6 = ["初心LOVE", "タンバルモリ", "ギャルピース", "片思いハート", "ちゅきちゅき"]
+    String_7 = ["わかりみが深い", "パワー!ヤー!", "ルーズソックス", "ガチおわなんよ", "ふざけんなよ!"]
+    String_8 = ["フレンチガーリー", "超ちるなラッパー", "どうかしてるぜっ", "あげていこうぜ!", "コーヒー無糖で。"]
+    String_9 =  ["イーアールサンスー", "アーニャピーナッツ", "人生詰んだじゃんw", "元カレがさ~クズ!", "ウチ辛いのマヂ無理"]
+    String_10 = ["ギャルちょーかわいー", "ひよってるやついる?", "パーティーしちゃう?", "ちょーかわいーじゃん", "キュンさせてもろて。"]
+
+    if num == 0:
+        reply_message = String_1[rand]
+    elif num == 1:
+        reply_message = String_2[rand]
+
+        if event.messege.text == "しね":
+            reply_message = "お前がな"
+    elif num == 2:
+        reply_message = String_3[rand]
+    elif num == 3:
+        reply_message = String_4[rand]
+    elif num == 4:
+        reply_message = String_5[rand]
+    elif num == 5:
+        reply_message = String_6[rand]
+    elif num == 6:
+        reply_message = String_7[rand]
+    elif num == 7:
+        reply_message = String_8[rand]
+    elif num == 8:
+        reply_message = String_9[rand]
+    elif num == 9:
+        reply_message = String_10[rand]
     elif num == 10:
         reply_message = "11文字送ってくんな!!"
     else:
